69dadf2a3232569e2b9e4e437e2e96728cc924f7
Mack <hypermail@yandex.ru>
2024-07-28 19:10:05 +0300

    orm

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from mainapp import models as mainapp_models
+from .models import Category, Client, Order, OrderItem, Product
 
-admin.site.register(mainapp_models.Product)
+admin.site.register(Client)
+admin.site.register(Category)
+admin.site.register(Product)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "created_at"]
+    inlines = [OrderItemInline]
--- mainapp/models.py
+++ mainapp/models.py
@@ -19,3 +19,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Заказ #{self.id} от {self.created_at}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="clients")
+
+    def __str__(self):
+        return f"{self.product.name} х {self.quantity} (Заказ #{self.order.id})"
